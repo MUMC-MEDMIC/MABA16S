6e14e764ed58e7377b7ac5df63a02769d090519a
Casper Jamin <34744774+Casperjamin@users.noreply.github.com>
2022-11-04 16:02:47 +0100

    kraken extract script

---
+++ maba16s/scripts/krakenextract.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+import sys
+
+
+def extract_reads(speciesID, name, krakenfile, genusID, fastqin, outdir):
+    os.system(f'mkdir -p {outdir}')
+    os.system(f'extract_kraken_reads.py -k {krakenfile} -o {outdir + "/" + name + ".fastq.gz"} -s {fastqin} -t {genusID}')
+
+
+def read_krakenreport(file, krakenfile, fastqin, outdir):
+    df = pd.read_csv(file, sep = '\t')
+    for i in df.iterrows():
+        info = i[1]
+        name = info[5].strip()
+        genusID = info[4]
+        extract_reads(genusID, name, krakenfile, genusID, fastqin, outdir)
+
+
+
+def main():
+    read_krakenreport(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+
+
+if __name__ == "__main__":
+    main()
