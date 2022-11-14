03dee7e0c1832df05e0f1c1905a8cd03ceffbc31
Casper Jamin <34744774+Casperjamin@users.noreply.github.com>
2022-11-14 09:33:44 +0100

    blasting to consensus

---
+++ maba16s/scripts/blast_consensus.py
@@ -0,0 +1,21 @@
+import sys
+import os
+import glob
+
+def main():
+    inputfasta = sys.argv[1]
+    threads = [2]
+    db = sys.argv[3]
+    output = sys.argv[4]
+
+    files = glob.glob(inputfasta + "*fasta")
+    print(files)
+    for fasta in files:
+        genusname = os.path.basename(fasta).split(".")[0]
+        print(genusname)
+        os.system(f'mkdir -p {output}')
+        os.system(f'blastn -db {db}/blastDB -query {fasta} -out {output}/{genusname}.txt')
+
+
+if __name__ == "__main__":
+    main()
