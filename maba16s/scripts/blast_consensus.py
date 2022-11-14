56ac2fbad3a0cfa800264ae77a9d46491a708c33
Casper Jamin <34744774+Casperjamin@users.noreply.github.com>
2022-11-14 17:00:53 +0100

    format no prints

--- maba16s/scripts/blast_consensus.py
+++ maba16s/scripts/blast_consensus.py
@@ -9,10 +9,8 @@ def main():
     output = sys.argv[4]
 
     files = glob.glob(inputfasta + "*fasta")
-    print(files)
     for fasta in files:
         genusname = os.path.basename(fasta).split(".")[0]
-        print(genusname)
         os.system(f'mkdir -p {output}')
         os.system(f'blastn -db {db}/blastDB -query {fasta} -out {output}/{genusname}.txt -outfmt 11')
 
