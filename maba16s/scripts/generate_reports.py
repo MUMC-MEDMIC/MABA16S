b2f3ff5811de06156114d612ea430d5da4ec47ad
Casper Jamin <34744774+Casperjamin@users.noreply.github.com>
2022-11-21 11:46:23 +0100

    snakefile reportgenerate

--- maba16s/scripts/generate_reports.py
+++ maba16s/scripts/generate_reports.py
@@ -1,9 +1,10 @@
 import sys
 import pandas as pd
 import glob
+import os
 
 percentage_from_max_bitscore = 0.90
-percentage_from_max_seqid = 1
+percentage_from_max_seqid = 0.35
 
 def read_blastn(file):
     df = pd.read_csv(file, sep="\t")
@@ -22,20 +23,43 @@ def score_top_hits(df):
     df = df[df.percentage > cutoff]
 
     df.taxonomy = df.taxonomy.apply(get_species)
-    print(str(df.taxonomy.value_counts()))
-    return df
+    data = {
+        "percentage":max(df.percentage),
+        "length":max(df.length),
+        "bitscore":max(df.bitscore),
+        "blast_hit":df.taxonomy.value_counts()
+    }
+    return data
 
 def get_species(value):
     species = value.split(";")[-1]
     return species
 
+def get_read_count(file):
+    data = pd.read_csv(file, sep="\t")
+    data.columns = ["percentage", "reads_in","reads_out", "taxlvl", "taxid", "name"]
+    data["name"] = data.name.apply(lambda x: x.strip())
+    data = data.set_index("name")
+    read_dict = data.to_dict()['reads_out']
+
+    return read_dict
+
 
 def main():
     blastindir = sys.argv[1]
-    outdir = sys.argv[2]
+    outxls = sys.argv[2]
+    readfile = sys.argv[3]
 
+    results = {}
     for file in glob.glob(blastindir + "/*consensus.txt"):
-        read_blastn(file)
+        data = read_blastn(file)
+        genus = os.path.basename(file).split("_consensus.txt")[0]
+        results[genus] = data
+
+    results = pd.DataFrame(results).T
+    readcount = get_read_count(readfile)
+    results['num_reads'] = [readcount[x] for x in results.index]
+    results.to_excel(outxls)
 
 
 
