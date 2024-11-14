144cf178dc5daa24ddaf008f0c4503a3b2d409c1
rkoeck <rebekka.koeck@mumc.nl>
2024-11-14 15:38:36 +0100

    corrected file type and removed non-alphanumeric characters from file name

--- maba16s/scripts/krakenextract.py
+++ maba16s/scripts/krakenextract.py
@@ -1,19 +1,18 @@
 import pandas as pd
 import os
 import sys
-
+import re
 
 def extract_reads(speciesID, name, krakenfile, genusID, fastqin, outdir):
     os.system(f'mkdir -p {outdir}')
-    os.system(f'extract_kraken_reads.py -k {krakenfile} -o {outdir + "/" + name + ".fastq.gz"} -s {fastqin} -t {genusID}')
+    os.system(f'extract_kraken_reads.py -k {krakenfile} -o {outdir + "/" + name + ".fasta"} -s {fastqin} -t {genusID}')
 
 
 def read_krakenreport(file, krakenfile, fastqin, outdir):
     df = pd.read_csv(file, sep = '\t', header = None)
-    #print(df)
     for i in df.iterrows():
         info = i[1]
-        name = info[5].strip()
+        name = re.sub(r'\W+', '', info[5])
         genusID = info[4]
         extract_reads(genusID, name, krakenfile, genusID, fastqin, outdir)
 
