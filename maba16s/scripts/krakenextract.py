61adc4c33503fa8b9334ab0d87fffe63ceb96e9b
rkoeck <rebekka.koeck@mumc.nl>
2025-07-03 15:10:28 +0200

    adjusted fasta file naming convention

--- maba16s/scripts/krakenextract.py
+++ maba16s/scripts/krakenextract.py
@@ -27,19 +27,21 @@ Arguments:
 import pandas as pd
 import os
 import sys
+import re
 
-def extract_reads(speciesID, name, krakenfile, genusID, fastqin, outdir):
+def extract_reads(name, krakenfile, genusID, fastqin, outdir):
     os.system(f'mkdir -p {outdir}')
-    os.system(f'extract_kraken_reads.py -k {krakenfile} -o "{outdir + "/" + name + ".fasta"}" -s {fastqin} -t {genusID}')
+    outfile = outdir + "/" + str(genusID) + "_" + name + ".fasta" 
+    os.system(f'extract_kraken_reads.py -k "{krakenfile}" -o "{outfile}" -s "{fastqin}" -t {genusID}')
 
 
 def read_krakenreport(file, krakenfile, fastqin, outdir):
     df = pd.read_csv(file, sep = '\t', header = None)
     for i in df.iterrows():
         info = i[1]
-        name = info[5].strip()
+        name = re.sub(r'[^A-Za-z0-9]', '', info[5])
         genusID = info[4]
-        extract_reads(genusID, name, krakenfile, genusID, fastqin, outdir)
+        extract_reads(name, krakenfile, genusID, fastqin, outdir)
 
 
 
